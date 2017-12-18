5151d3a37f44a8f4a5b2a6cd9ae06c99bf701ef4
James Curran <j.curran@auckland.ac.nz>
2017-12-18 16:14:51 +1300

    Changed readProfiles to allow the on the fly calculation of the frequencies

--- R/readProfiles.R
+++ R/readProfiles.R
@@ -8,20 +8,24 @@
 #' @param fileName a path to the profile file.
 #' @param freqs A list containing two lists labelled loci and freqs. The second
 #' list is a list of vectors containing the allele frequencies of each allele
-#' at each locus in the multiplex.
-#' @param delim a character that delimits the fields in the profile file.
+#' at each locus in the multiplex. If this is left \code{NULL}, then it is calculated from 
+#' the profile file.
+#' @param sep a character that delimits the fields in the profile file.
 #' @param header a boolean witch is \code{TRUE} if the profile file has a column header line.
 #' @param id a column number indicating which column the profile id's are stored. If \code{id == -1}, then this means there is no id information.
-#' @return a \code{data.frame} of profiles where the alleles have been recoded to the allele index number, rather than the allele itself.
+#' @return a list containing a \code{data.frame} of profiles where the alleles have been recoded 
+#' to the allele index number, rather than the allele itself, and a set of frequencies in the same
+#' format as you would get from \code{\link{readFreqs}}. If \code{freqs} have been supplied, then this 
+#' will just be the same set of frequencies, if they have not, then this will be calculated
+#' from the profiles. Given that the profiles generally do not have any locus name information
+#' the loci will just be labelled Locus1, Locus2, \ldots.
 #' @author James M. Curran
 #' @examples
 #'
-#' data(fbiCaucs)
-#'
 #' @export readProfiles
 readProfiles = function(fileName,
-                        freqs,
-                        delim = "\t",
+                        freqs = NULL,
+                        sep = "\t",
                         header = FALSE,
                         id = 1) {
   if (header) {
@@ -30,7 +34,7 @@ readProfiles = function(fileName,
                       header = FALSE,
                       skip = 1)
   } else{
-    prof = read.table(fileName, sep = delim, header = FALSE)
+    prof = read.table(fileName, sep = sep, header = FALSE)
   }
   
   idCol = NULL
@@ -38,6 +42,26 @@ readProfiles = function(fileName,
     idCol = prof[, id]
     prof = prof[, -id]
   }
+  
+  if(is.null(freqs)){
+    if(ncol(prof) %% 2 != 0){
+      Stop("The profile file must contain an even number of columns excluding the id column\n")
+    }else{
+      numLoci = ncol(prof) / 2
+      
+      freqs$loci = paste("Locus", 1:numLoci)
+      freqs$freqs = vector(mode = "list", length = numLoci)
+      names(freqs$freqs) = freqs$loci
+      
+      for(nloc in 1:numLoci){
+        i1 = 2 * nloc - 1
+        i2 = i1 + 2
+        freqs$freqs[[nloc]] = table(c(prof[,i1], prof[,i2]))
+        freqs$freqs[[nloc]] = freqs$freqs[[nloc]] / sum(freqs$freqs[[nloc]])
+      }
+    }
+  }
+  
   numLoci = length(freqs$freqs)
   
   for (loc in 1:numLoci) {
@@ -52,5 +76,5 @@ readProfiles = function(fileName,
   
   rownames(prof) = 1:nrow(prof)
   
-  return(prof)
+  return(list(profiles = prof, freqs = freqs))
 }

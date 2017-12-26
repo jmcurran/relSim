40a87f8c7e99d6b91d6f84ce939e80c0c85773ec
jmcurran <j.curran@auckland.ac.nz>
2017-12-27 12:55:05 +1300

    pre-CRAN submit

--- src/R/testIS.R
+++ src/R/testIS.R
@@ -1,8 +1,9 @@
 testIS = function(nc = c(3, 2), locus = 1, seed = 123456){
   set.seed(seed)
   np = 2 * nc[2]
+  freqs = USCaucs$freqs
   for(i in 2:np){
-    cmd = paste0("p",i," = IS(USCaucs$freqs[locus], numContributors = nc[1], maxPeaks =  ", i,", numIterations = 1e5)")
+    cmd = paste0("p",i," = IS(freqs[locus], numContributors = nc[1], maxPeaks =  ", i,", numIterations = 1e5)")
     cat(cmd, "\n")
     eval(parse(text = cmd))
     
@@ -10,6 +11,7 @@ testIS = function(nc = c(3, 2), locus = 1, seed = 123456){
     eval(parse(text = cmd))
   }
   
+  est = NULL
   cmd = paste0("est = c(", paste0("p", 1:np, "$est", collapse = ", "), ")")
   eval(parse(text = cmd))
   cat(paste(est, "\n"))
